7e591dd502d88afedc510838b7abe745a4a1d3bb
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-15 11:43:46 -0400

    add event emitting to `App` example

--- examples/app.js
+++ examples/app.js
@@ -1,23 +1,45 @@
+var Emitter = require('component-emitter');
 var visit = require('collection-visit');
+
+var colors = {
+  warn: require('ansi-yellow'),
+  info: require('ansi-cyan'),
+  success: require('ansi-green'),
+  error: require('ansi-red')
+};
+
+var action = {
+  set: 'success',
+  get: 'info',
+  del: 'error'
+};
 
 /**
  * Example application, used in the other examples
  */
 
 function App(cache) {
+  Emitter.call(this);
   this.cache = cache || {};
 }
+
+Emitter(App.prototype);
 
 App.prototype.set = function(key, value) {
   if (typeof key === 'object') {
     return this.visit('set', key);
   }
   this.cache[key] = value;
+  this.emit('set', key, value);
+  this.logEvent('set')(key, value);
   return this;
 };
 
 App.prototype.get = function(key) {
-  return this.cache[key];
+  var res = this.cache[key];
+  this.emit('get', key, res);
+  this.logEvent('get')(key, res);
+  return res;
 };
 
 App.prototype.del = function(key) {
@@ -25,7 +47,19 @@ App.prototype.del = function(key) {
     return this.visit('del', key);
   }
   delete this.cache[key];
+  this.emit('del', key);
+  this.logEvent('del')(key);
   return key;
+};
+
+App.prototype.logEvent = function(method) {
+  return function () {
+    var args = [].slice.call(arguments);
+    args.unshift(colors[action[method]](method));
+    var str = JSON.stringify(args, null, 2);
+    console.log.apply(console, args);
+    return str;
+  };
 };
 
 App.prototype.has = function(key) {
@@ -33,7 +67,15 @@ App.prototype.has = function(key) {
 };
 
 App.prototype.visit = function(method, val) {
-  visit(this, method, val);
+  if (Array.isArray(method)) {
+    method.map(this.visit.bind(this));
+  } else if (typeof method === 'object') {
+    for (var key in method) {
+      visit(this, key, method[key]);
+    }
+  } else {
+    visit(this, method, val);
+  }
   return this;
 };
 
